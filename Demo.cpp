73b4fe9b0a80c5f8b671a1fee1aa9d4d1cbca2f5
Tushar443 <moretushart33@gmail.com>
2021-01-03 11:48:52 +0530

    .

--- Demo.cpp
+++ Demo.cpp
@@ -1,6 +1,15 @@
 #include<iostream>
 using namespace std;
 int main(){
+    int a = 20;
+    int &n= a;
+    cout<<&n<<endl;
+    cout<<&a<<endl;
+    n=a++;
+    cout<<"7 "<<a<<endl;
+    a=n++;
+    
 
+    cout<<a<<" "<<n<<endl;
     return 0;
 }
