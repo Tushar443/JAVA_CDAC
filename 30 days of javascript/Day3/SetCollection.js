8abf6ef03d0476204840387dd01028faf3ae7995
Tushar More <Tushar.More@sapiens.com>
2022-06-23 17:03:02 +0530

    adding day2 and day3 folder

---
+++ 30 days of javascript/Day3/SetCollection.js
@@ -0,0 +1,13 @@
+const languages = [
+    'English',
+    'Finnish',
+    'English',
+    'French',
+    'Spanish',
+    'English',
+    'French',
+  ]
+
+  const setexm = new Set(languages);
+  console.log(setexm);
+  
