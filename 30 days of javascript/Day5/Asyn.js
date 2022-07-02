a58c2f468deb5c373ff33189ef8260754cf3d9b8
Tushar More <Tushar.More@sapiens.com>
2022-07-02 13:58:51 +0530

    Move javascript code

--- 30 days of javascript/Day5/Asyn.js
+++
@@ -1,5 +0,0 @@
-const square = async function(n){
-    return n*n;
-}
-
-square(8).then(res => console.log(res)).catch(err => console.log(err))
--- 30 days of javascript/Day5/HTMLWebStorage.js
+++
@@ -1,5 +0,0 @@
-// localStorage.getItem('firstName');
-// localStorage.setItem('firstName','Tushar');
-// localStorage.key('firstName');
-// localStorage.removeItem('firstName');
-// localStorage.clear();
